651791ec8a4ace906beb82c0ae9ff0ebc985238b
TiFAu <49918680+TiFAu@users.noreply.github.com>
2019-06-11 02:01:34 +0300

    Add files via upload

---
+++ lesson-02/Additionally.js
@@ -0,0 +1,5 @@
+function dataTypeChecking (variable){
+                                        var timeNow = new Date ();
+                                        Number ( variable ) ? console.log(timeNow.toLocaleString()) : console.log("Неверный тип данных")                
+                                    }
+
