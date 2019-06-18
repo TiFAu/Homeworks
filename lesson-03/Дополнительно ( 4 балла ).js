6198a59c0169cf6ad4201233267513802301faf1
TiFAu <turchin.anton2@gmail.com>
2019-06-18 22:16:28 +0300

    Last changes

---
+++ lesson-03/Дополнительно ( 4 балла ).js
@@ -0,0 +1,37 @@
+var objectMyOne =   {
+                    name: "Антон",
+                    data: "22.8.1979",
+                    registered: true
+                    }
+var objectMyTwo = {
+                    name: "Галина",
+                    data: "04.8.1986",
+                    registered: true
+                    }
+var objectMyThree = {
+            name: "Коля",
+            data: "",
+            registered: false
+            }
+
+
+function userInfo(object){  
+                            this.name = object.name;
+                            this.data = object.data;
+                            this.registered = object.registered;
+                            console.log(
+                                        this.registered ? `Дата регистрации : ${this.data}` : `Незарегистрированный пользователь : ${this.name}`
+                                        )
+                        }
+objectMyOne.getInfo = userInfo;
+objectMyTwo.getInfo = userInfo;
+bjectMyThree.getInfo = userInfo;
+/////////////////////////////////
+
+var objectMyTwo = Object.create(objectMyOne);
+objectMyTwo.name = "Галина";
+objectMyTwo.data = "04.8.1986";
+objectMyTwo.registered = true;
+objectMyOne.getInfo = userInfo;
+objectMyTwo.getInfo = userInfo;
+ 
---
+++ lesson-03/Обязательная часть ( 3 балла ).js
@@ -0,0 +1,4 @@
+function say ( ) {
+                    (arguments.length === 0) ? console.dir ( say ) : console.log ( say )
+                    }
+say (10, false, "google")
